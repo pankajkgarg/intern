d3d43b67102940d808455bb083c0f280c182e347
Pankaj Garg <pankajn17@gmail.com>
2011-07-26 15:27:12 +0100

    Modified apache.py

--- apache.py
+++ apache.py
@@ -4,4 +4,9 @@ import web
 curdir = os.path.dirname(__file__)
 session = web.session.Session(app, web.session.DiskStore(os.path.join(curdir,'sessions')),)
 
+import sys
+sys.stdout = sys.stderr
+
+
+
 application = app.wsgifunc()
